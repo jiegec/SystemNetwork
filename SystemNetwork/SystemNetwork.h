ab954989d8c06364f514104aebe887535e65c642
Jiajie Chen <jiegec@qq.com>
2018-09-08 02:16:13 +0800

    Initial Commit

---
+++ SystemNetwork/SystemNetwork.h
@@ -0,0 +1,15 @@
+//
+//  SystemNetwork.h
+//  SystemNetwork
+//
+//  Created by MacBookAir on 2018/9/8.
+//  Copyright © 2018 jiegec. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface SystemNetwork : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ SystemNetwork/SystemNetwork.m
@@ -0,0 +1,17 @@
+//
+//  SystemNetwork.m
+//  SystemNetwork
+//
+//  Created by MacBookAir on 2018/9/8.
+//  Copyright © 2018 jiegec. All rights reserved.
+//
+
+#import "SystemNetwork.h"
+
+@implementation SystemNetwork
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
