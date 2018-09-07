afdd17a8b582cb0ae028d76ba0ce39f6d9ad0d25
Jiajie Chen <jiegec@qq.com>
2018-09-08 03:40:42 +0800

    Extract Network services from system

--- SystemNetwork/SystemNetwork.h
+++ SystemNetwork/SystemNetwork.h
@@ -3,13 +3,16 @@
 //  SystemNetwork
 //
 //  Created by MacBookAir on 2018/9/8.
-//  Copyright © 2018 jiegec. All rights reserved.
+//  Copyright © 2018 Jiajie Chen. All rights reserved.
 //
 
 #import <PreferencePanes/PreferencePanes.h>
 
-@interface SystemNetwork : NSPreferencePane
+@interface SystemNetwork : NSPreferencePane <NSTableViewDelegate, NSTableViewDataSource>
 
 - (void)mainViewDidLoad;
+@property (weak) IBOutlet NSTableView *networkServicesTableView;
+
+@property (strong) NSMutableDictionary *config;
 
 @end
--- SystemNetwork/SystemNetwork.m
+++ SystemNetwork/SystemNetwork.m
@@ -3,15 +3,48 @@
 //  SystemNetwork
 //
 //  Created by MacBookAir on 2018/9/8.
-//  Copyright © 2018 jiegec. All rights reserved.
+//  Copyright © 2018 Jiajie Chen. All rights reserved.
 //
 
 #import "SystemNetwork.h"
 
 @implementation SystemNetwork
 
+static NSString * CONFIG_PATH = @"/Library/Preferences/SystemConfiguration/preferences.plist";
+
 - (void)mainViewDidLoad
 {
+    NSData *data = [NSData dataWithContentsOfFile:CONFIG_PATH];
+    [self setConfig:(NSMutableDictionary *)[NSPropertyListSerialization propertyListWithData:data options:NSPropertyListMutableContainersAndLeaves format:nil error:nil]];
+    NSLog(@"%@", [self config]);
+    
+    [[self networkServicesTableView] setDelegate:self];
+    [[self networkServicesTableView] setDataSource:self];
+}
+
+
+#pragma mark -
+#pragma mark NSTableViewDataSource Implementation
+
+-  (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return [[self config][@"NetworkServices"] count];
+}
+
+#pragma mark -
+#pragma mark NSTableViewDelegate Implementation
+
+- (CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row {
+    return 25;
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    NSTextField *textField = [[NSTextField alloc] init];
+    [textField setEditable:NO];
+    NSDictionary *services = [self config][@"NetworkServices"];
+    NSArray *keys = [services allKeys];
+    [textField setStringValue:services[keys[row]][@"UserDefinedName"]];
+    
+    return textField;
 }
 
 @end
